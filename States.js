333dfca3ba4f6ee05a18ecb18dcaf49142c65ec1
Alphabeard <theawesomeguynextdoor@gmail.com>
2020-09-05 12:05:02 -0700

    fixed joining game without username

--- States.js
+++ States.js
@@ -1,202 +1,222 @@
 class Message {
-    constructor(event) {
-        this.event = event;
-        this.playerData = null;
-        this.playersData = null;
-        this.img = null;
-    }
+  constructor(event) {
+    this.event = event;
+    this.playerData = null;
+    this.playersData = null;
+    this.img = null;
+  }
 }
 
 class State {
-    constructor(state, room) {
-        this.timerInterval = null;
-        this.duration = null;
-        this.state = state;
-        this.room = room;
-    }
-    /**
-     * @description Starts a timeout for the state and sends a custom message to the client if specified
-     * @param {*} Duration 
-     * @param {*} Outgoing_Message 
-     */
-    start(duration, OUT_MSG) {
-        this.duration = duration
-        if (!OUT_MSG) OUT_MSG = new Message(this.state);
-        OUT_MSG.round = this.room.round;
-        this.room.sendGameMessage(OUT_MSG);
-        if (duration == -1) return;
+  constructor(state, room) {
+    this.timerInterval = null;
+    this.duration = null;
+    this.state = state;
+    this.room = room;
+  }
+  /**
+   * @description Starts a timeout for the state and sends a custom message to the client if specified
+   * @param {*} Duration
+   * @param {*} Outgoing_Message
+   */
+  start(duration, OUT_MSG) {
+    this.duration = duration;
+    if (!OUT_MSG) OUT_MSG = new Message(this.state);
+    OUT_MSG.round = this.room.round;
+    this.room.sendGameMessage(OUT_MSG);
+    if (duration == -1) return;
 
-        this.timerInterval = setInterval(() => {
-            this.duration--;
-            if (this.duration <= 0) {
-                clearInterval(this.timerInterval);
-                this.end();
-            }
-        }, 1000);
-    }
-    /**
-     * @description The supermethod for parsing client messages. Messages parsed here must do the same thing in every state i.e. when a player joins, a card is always added to the lobby.
-     * @param {*} Incoming_Message 
-     */
-    parseMessage(IN_MSG) {
-        console.log(IN_MSG);
-        switch (IN_MSG.event) {
-            case ('leaveRoom'):
-                this.room.removePlayer(IN_MSG.playerData.name);
-                const OUT_MSG = new Message('playerLeft');
-                OUT_MSG.playerData = IN_MSG.playerData;
-                this.room.sendGameMessage(OUT_MSG);
-                break;
-        }
-    }
-    end() {
+    this.timerInterval = setInterval(() => {
+      this.duration--;
+      if (this.duration <= 0) {
         clearInterval(this.timerInterval);
+        this.end();
+      }
+    }, 1000);
+  }
+  /**
+   * @description The supermethod for parsing client messages. Messages parsed here must do the same thing in every state i.e. when a player joins, a card is always added to the lobby.
+   * @param {*} Incoming_Message
+   */
+  parseMessage(IN_MSG) {
+    console.log(IN_MSG);
+    switch (IN_MSG.event) {
+      case "joinedServer": {
+        break;
+      }
+      case "leaveRoom":
+        this.room.removePlayer(IN_MSG.playerData.name);
+        const OUT_MSG = new Message("playerLeft");
+        OUT_MSG.playerData = IN_MSG.playerData;
+        this.room.sendGameMessage(OUT_MSG);
+        break;
     }
+  }
+  end() {
+    clearInterval(this.timerInterval);
+  }
 }
 
 class Lobby extends State {
-    constructor(room) {
-        super('Lobby', room);
-    }
-    parseMessage(msg, socket) {
-        super.parseMessage(msg);
-        const playerData = msg.playerData;
-        const playersData = msg.playersData;
-        const room = this.room;
-        switch (msg.event) {
-            case ('joinedServer'):
-                room.addPlayer(playerData.name, playerData.lead);
-                socket.join(room.id);
-                // Send player who joined all players
-                const msg = new Message('loadPlayers');
-                msg.playersData = room.players;
-                socket.emit('messageFromServer', msg);
-                room.print();
-                break;
-
-            case ('hostRequestStart'):
-                this.end();
-                break;
+  constructor(room) {
+    super("Lobby", room);
+  }
+  parseMessage(msg, socket) {
+    super.parseMessage(msg);
+    const playerData = msg.playerData;
+    const playersData = msg.playersData;
+    const room = this.room;
+    switch (msg.event) {
+      case "joinedServer":
+        if (playerData.name === "") {
+          const OUT_MSG = new Message("redirect");
+          OUT_MSG.url = "/";
+          OUT_MSG.roomExt = "" + this.room.id;
+          socket.emit("redirect", OUT_MSG);
+          return;
         }
+        room.addPlayer(playerData.name, playerData.lead);
+        socket.join(room.id);
+        // Send player who joined all players
+        const msg = new Message("loadPlayers");
+        msg.playersData = room.players;
+        socket.emit("messageFromServer", msg);
+        room.print();
+        break;
+
+      case "hostRequestStart":
+        this.end();
+        break;
     }
-    end() {
-        super.end()
-        this.room.state = new Start(this.room);
-        this.room.state.start(5, null);
-        this.room.private = true;
-    }
+  }
+  end() {
+    super.end();
+    this.room.state = new Start(this.room);
+    this.room.state.start(5, null);
+    this.room.private = true;
+  }
 }
 
 class Start extends State {
-    constructor(room) {
-        super('Start', room);
-    }
-    start(duration, OUT_MSG) {
-        this.room.round++;
-        super.start(duration, OUT_MSG);
-    }
-    async end() {
-        super.end();
-        this.room.state = new Submission(this.room);
-        this.room.state.start();
-    }
+  constructor(room) {
+    super("Start", room);
+  }
+  start(duration, OUT_MSG) {
+    this.room.round++;
+    super.start(duration, OUT_MSG);
+  }
+  async end() {
+    super.end();
+    this.room.state = new Submission(this.room);
+    this.room.state.start();
+  }
 }
 class Submission extends State {
-    constructor(room) {
-        super('Submission', room);
-        this.answersSubmitted = 0;
-        room.resetVotes();
-    }
-    async start() {
-        const OUT_MSG = new Message('Submission');
-        OUT_MSG.img = await this.room.getMeme();
-        super.start(100, OUT_MSG);
-    }
-    parseMessage(IN_MSG) {
-        super.parseMessage(IN_MSG);
+  constructor(room) {
+    super("Submission", room);
+    this.answersSubmitted = 0;
+    room.resetVotes();
+  }
+  async start() {
+    const OUT_MSG = new Message("Submission");
+    OUT_MSG.img = await this.room.getMeme();
+    super.start(100, OUT_MSG);
+  }
+  parseMessage(IN_MSG) {
+    super.parseMessage(IN_MSG);
 
-        switch (IN_MSG.event) {
-            case ('submitAnswer'): {
-                const SERVER_PLAYER = this.room.getPlayerByName(IN_MSG.playerData.name);
-                SERVER_PLAYER.addAnswer(IN_MSG.answer);
-                this.answersSubmitted++;
+    switch (IN_MSG.event) {
+      case "submitAnswer": {
+        const SERVER_PLAYER = this.room.getPlayerByName(IN_MSG.playerData.name);
+        SERVER_PLAYER.addAnswer(IN_MSG.answer);
+        this.answersSubmitted++;
 
-                const OUT_MSG = new Message('answerSubmitted');
-                OUT_MSG.answer = IN_MSG.answer;
-                OUT_MSG.playerData = SERVER_PLAYER;
-                this.room.sendGameMessage(OUT_MSG);
+        const OUT_MSG = new Message("answerSubmitted");
+        OUT_MSG.answer = IN_MSG.answer;
+        OUT_MSG.playerData = SERVER_PLAYER;
+        this.room.sendGameMessage(OUT_MSG);
 
-                if (this.answersSubmitted == this.room.players.length) this.end();
-            }
-        }
+        if (this.answersSubmitted == this.room.players.length) this.end();
+      }
     }
-    end() {
-        super.end();
-        this.room.state = new Voting(this.room);
-        this.room.state.start();
-    }
+  }
+  end() {
+    super.end();
+    this.room.state = new Voting(this.room);
+    this.room.state.start();
+  }
 }
 class Voting extends State {
-    constructor(room) {
-        super('Voting', room);
-        this.votesSubmitted = 0;
+  constructor(room) {
+    super("Voting", room);
+    this.votesSubmitted = 0;
+  }
+  start() {
+    const OUT_MSG = new Message("Voting");
+    OUT_MSG.playersData = this.room.players;
+    super.start(50, OUT_MSG);
+  }
+  parseMessage(IN_MSG) {
+    super.parseMessage(IN_MSG);
+    switch (IN_MSG.event) {
+      case "submitVote":
+        this.votesSubmitted++;
+        this.room.addVote(IN_MSG.playerData);
+        if (this.votesSubmitted == this.room.players.length) this.end();
+        break;
     }
-    start() {
-        const OUT_MSG = new Message('Voting');
-        OUT_MSG.playersData = this.room.players;
-        super.start(50, OUT_MSG);
-    }
-    parseMessage(IN_MSG) {
-        super.parseMessage(IN_MSG);
-        switch (IN_MSG.event) {
-            case ('submitVote'):
-                this.votesSubmitted++;
-                this.room.addVote(IN_MSG.playerData);
-                if (this.votesSubmitted == this.room.players.length) this.end();
-                break;
-        }
-    }
-    async end() {
-        super.end();
-        this.room.state = new Score(this.room);
-        this.room.state.start();
-    }
+  }
+  async end() {
+    super.end();
+    this.room.state = new Score(this.room);
+    this.room.state.start();
+  }
 }
 class Score extends State {
-    constructor(room) {
-        super('Score', room);
+  constructor(room) {
+    super("Score", room);
+  }
+  start() {
+    // Get Every player that scored
+    const OUT_MSG = new Message("Score");
+    OUT_MSG.playersData = this.room.players;
+    super.start(10, OUT_MSG);
+  }
+  end() {
+    super.end();
+    if (this.room.round == 8) {
+      this.room.state = new Results(this.room);
+      this.room.state.start();
+      return;
     }
-    start() {
-        // Get Every player that scored
-        const OUT_MSG = new Message('Score');
-        OUT_MSG.playersData = this.room.players;
-        super.start(10, OUT_MSG);
-    }
-    end() {
-        super.end();
-        if(this.room.round == 8){
-            this.room.state = new Results(this.room);
-            this.room.state.start();
-            return;
-        }
-        this.room.state = new Start(this.room);
-        this.room.state.start(5, null);
-    }
+    this.room.state = new Start(this.room);
+    this.room.state.start(5, null);
+  }
 }
-class Results extends State{
-    constructor(room) {
-        super('Results', room);
+class Results extends State {
+  constructor(room) {
+    super("Results", room);
+    this.votes = 0;
+  }
+  start() {
+    const OUT_MSG = new Message("Results");
+    OUT_MSG.playersData = this.room.players;
+    super.start(-1, OUT_MSG);
+  }
+  parseMessage(IN_MSG) {
+    super.parseMessage(IN_MSG);
+    switch (IN_MSG.event) {
+      case "requestPlayAgain": {
+        this.votes++;
+        if (this.votes >= this.room.players.length / 2) {
+          this.end();
+        }
+        break;
+      }
     }
-    start(){
-        const OUT_MSG = new Message('Results');
-        OUT_MSG.playersData = this.room.players;
-        super.start(-1, OUT_MSG);
-    }
-    parseMessage(IN_MSG){
-        
-    }
-    end(){
-
-    }
+  }
+  end() {
+    this.room.state = new Lobby(this.room);
+  }
 }
 module.exports = { State, Lobby, Start, Submission };
--- public/js/game.js
+++ public/js/game.js
@@ -1,4 +1,4 @@
-const urlExt = window.location.href.split('/')[3];
+const urlExt = window.location.href.split("/")[3];
 // STATES = {
 //     LOBBY: 1,
 //     START: 2,
@@ -9,177 +9,197 @@ const urlExt = window.location.href.split('/')[3];
 // }
 
 class Message {
-    constructor(event) {
-        let me = { ...room.myPlayer };
-        me.card = null;
-        me.submissionCard = null;
-        me.answerCard = null;
-        this.event = event;
-        this.roomID = urlExt;
-        this.playerData = me;
-        this.playersData = null
-    }
+  constructor(event) {
+    let me = { ...room.myPlayer };
+    me.card = null;
+    me.submissionCard = null;
+    me.answerCard = null;
+    this.event = event;
+    this.roomID = urlExt;
+    this.playerData = me;
+    this.playersData = null;
+  }
 }
 
 class Player {
-    constructor(name, card, lead) {
-        this.name = name;
-        this.card = card;
-        this.lead = lead;
-        this.vote = undefined;
-        this.answerCard = undefined;
-        this.points = 0;
-        this.roundVotes = 0;
-    }
-    addHostPrivilege() {
-        console.log('You are the host!')
-        let startBtn = ElementCreate.startButton();
-        $('.game-content').append(startBtn);
-    }
-    addPoints(playerData) {
-        this.roundVotes = playerData.roundVotes;
-    }
-    pointAnimation() {
-        this.card.addClass('scored');
-        this.points += 100;
-        const score = 'Score: ' + this.points;
-        this.card.children().text(score);
-        document.getElementById('score_audio').play();
-        setTimeout(() => { this.card.removeClass('scored'); }, 1000);
-    }
-    remove() {
-        this.card.remove();
-    }
-    createAnimateResultCard() {
-        const MAX_HEIGHT = 700;
-        const CARD_ID = '#' + this.name + 'result-card';
-        let i = this.points / 100, score = 0;
-        $('#score-overlay').append(ElementCreate.resultCard(this.name));
-        this.interval = setInterval(() => {
-            if (!i) { clearInterval(this.interval); return; }
-            i--;
+  constructor(name, card, lead) {
+    this.name = name;
+    this.card = card;
+    this.lead = lead;
+    this.vote = undefined;
+    this.answerCard = undefined;
+    this.points = 0;
+    this.roundVotes = 0;
+  }
+  addHostPrivilege() {
+    console.log("You are the host!");
+    let startBtn = ElementCreate.startButton();
+    $(".game-content").append(startBtn);
+  }
+  addPoints(playerData) {
+    this.roundVotes = playerData.roundVotes;
+  }
+  pointAnimation() {
+    this.card.addClass("scored");
+    this.points += 100;
+    const score = "Score: " + this.points;
+    this.card.children().text(score);
+    document.getElementById("score_audio").play();
+    setTimeout(() => {
+      this.card.removeClass("scored");
+    }, 1000);
+  }
+  remove() {
+    this.card.remove();
+  }
+  createAnimateResultCard() {
+    const MAX_HEIGHT = 700;
+    const CARD_ID = "#" + this.name + "result-card";
+    let i = this.points / 100,
+      score = 0;
+    $(".scores").append(ElementCreate.resultCard(this.name));
+    this.interval = setInterval(() => {
+      if (!i) {
+        clearInterval(this.interval);
+        return;
+      }
+      i--;
 
-            const currentHeight = $(CARD_ID).height();
-            $(CARD_ID).css('height', currentHeight + 65);
-            $(CARD_ID + ' h1').text(score += 100);
-        }, 500);
-    }
-
+      const currentHeight = $(CARD_ID).height();
+      $(CARD_ID).css("height", currentHeight + 65);
+      $(CARD_ID + " h1").text((score += 100));
+    }, 500);
+  }
 }
 
 class Room {
-    constructor(socket) {
-        this.players = [];
-        this.socket = socket;
-        this.myPlayer = null;
-        this.myUsername = getCookie('username');
-        this.$board = $('.game-content');
-        this.memes = [];
-        this.state = new Lobby(this);
-        this.round = 0;
-    }
-    addPlayer(name, lead) {
-        document.getElementById('blip_audio').play();
-        const playerCard = ElementCreate.playerCard(name, { host: lead, score: 0 });
-        $('.players').append(playerCard);
-        let player = new Player(name, playerCard, lead);
-        this.players.push(player);
-        return player;
-    }
-    getPlayerByName(name) {
-        return this.players.find((player) => {
-            return player.name == name;
-        });
-    }
-    removePlayer(name) {
-        const player = this.getPlayerByName(name);
-        player.remove();
-        this.players = this.players.filter(player => {
-            return player.name != name;
-        });
-    }
-    addWaitPrompt() {
-        this.waitPrompt = new WaitPrompt();
-    }
-    startRoundAnimations() {
-        const roundIntroString = `round${this.round}_intro`;
-        console.log(roundIntroString);
-        document.getElementById(roundIntroString).play();
-        document.getElementById('wait_music').play();
-        $('#round-overlay h1').text(`Round ${this.round}`);
-        $('#round-overlay').show().css('left', '0%');
+  constructor(socket) {
+    this.players = [];
+    this.socket = socket;
+    this.myPlayer = null;
+    this.myUsername = getCookie("mama_meme_username");
+    this.$board = $(".game-content");
+    this.memes = [];
+    this.state = new Lobby(this);
+    this.round = 0;
+  }
+  addPlayer(name, lead) {
+    document.getElementById("blip_audio").play();
+    const playerCard = ElementCreate.playerCard(name, { host: lead, score: 0 });
+    $(".players").append(playerCard);
+    let player = new Player(name, playerCard, lead);
+    this.players.push(player);
+    return player;
+  }
+  getPlayerByName(name) {
+    return this.players.find((player) => {
+      return player.name == name;
+    });
+  }
+  removePlayer(name) {
+    const player = this.getPlayerByName(name);
+    player.remove();
+    this.players = this.players.filter((player) => {
+      return player.name != name;
+    });
+  }
+  addWaitPrompt() {
+    this.waitPrompt = new WaitPrompt();
+  }
+  startRoundAnimations() {
+    const roundIntroString = `round${this.round}_intro`;
+    console.log(roundIntroString);
+    document.getElementById(roundIntroString).play();
+    document.getElementById("wait_music").play();
+    $("#round-overlay h1").text(`Round ${this.round}`);
+    $("#round-overlay").show().css("left", "0%");
+    setTimeout(() => {
+      $("#round-overlay").css("left", "100%");
+      this.$board.append(ElementCreate.roundTitle(this.round));
+    }, 5000);
+  }
+  displayRoundScore() {
+    let t = 2000,
+      dt = 2000;
+    this.players.forEach((player) => {
+      const votes = player.roundVotes;
+      for (let i = 0; i < votes; i++) {
         setTimeout(() => {
-            $('#round-overlay').css('left', '100%');
-            this.$board.append(ElementCreate.roundTitle(this.round));
-        }, 5000);
-    }
-    displayRoundScore() {
-        let t = 2000, dt = 2000;
-        this.players.forEach((player) => {
-            const votes = player.roundVotes;
-            for (let i = 0; i < votes; i++) {
-                setTimeout(() => { player.pointAnimation(); }, t);
-                t += dt;
-            }
-        });
-    }
-    displayGameResults() {
-        $('#score-overlay').css('display', 'flex');
-        setTimeout(() => { $('#score-overlay').css('top', '0'); }, 100);
-        let t = 1500, dt = 2000;
-        this.players.forEach(player => {
-            setTimeout(() => { player.createAnimateResultCard(); }, t);
-            t += dt;
-        });
-    }
-    requestStart() {
-        $('#start-game-button').hide();
-        const OUT_MSG = new Message('hostRequestStart');
-        this.sendServerMessage(OUT_MSG);
-    }
-    submitAnswer() {
-        const OUT_MSG = new Message('submitAnswer');
-        OUT_MSG.answer = $('#user-answer').val();
-        $('.card.answer-input').remove();
-        this.sendServerMessage(OUT_MSG);
-    }
-    parseMessage(message) {
-        this.state.parseMessage(message)
-    }
-    loadSubmissionElements() {
-        document.getElementById('woosh_audio').play();
-        $('body').addClass('hide-overflow');
-        setTimeout(() => { $('body').removeClass('hide-overflow'); }, 1000);
-        $('.meme-format-container').append(ElementCreate.meme(this.memes[0]));
-        this.$board.append(ElementCreate.answerCard());
-    }
-    addHiddenSubmission(answer, playerName) {
-        document.getElementById('bleep_audio').play();
-        let player = this.getPlayerByName(playerName);
-        let card = new Card(answer, player, this);
-        player.submissionCard = card;
-        $('.meme-answer-container').append(card.element);
-        $('.vote-prompt').removeClass('hidden');
-    }
-    loadVotingElements() {
-        let t = 2000, dt = 4000;
-        this.players.forEach((player) => {
-            setTimeout(() => {
-                player.submissionCard.reveal();
-            }, t);
-            t += dt;
-        });
-    }
-    clearVotingElements() {
-        $('.round-title').remove();
-        $('.meme-format').remove();
-        $('.reveal-card').remove();
-        $('.vote-prompt').addClass('hidden');
-        $('#answer-input').remove();
-        $('#round-overlay').css('left', '-100%').hide();
-    }
-    sendServerMessage(OUT_MSG) {
-        this.socket.emit('messageFromClient', OUT_MSG);
-    }
-
+          player.pointAnimation();
+        }, t);
+        t += dt;
+      }
+    });
+  }
+  displayGameResults() {
+    $("#score-overlay").css("display", "flex");
+    setTimeout(() => {
+      $("#score-overlay").css("top", "0");
+    }, 100);
+    let t = 1500,
+      dt = 2000;
+    this.players.forEach((player) => {
+      setTimeout(() => {
+        player.createAnimateResultCard();
+      }, t);
+      t += dt;
+    });
+  }
+  requestStart() {
+    $("#start-game-button").hide();
+    const OUT_MSG = new Message("hostRequestStart");
+    this.sendServerMessage(OUT_MSG);
+  }
+  submitAnswer() {
+    const OUT_MSG = new Message("submitAnswer");
+    OUT_MSG.answer = $("#user-answer").val();
+    $(".card.answer-input").remove();
+    this.sendServerMessage(OUT_MSG);
+  }
+  requestPlayAgain() {
+    $("#play-again-button").prop("disabled", true);
+    const OUT_MSG = new Message("requestPlayAgain");
+    this.sendServerMessage(OUT_MSG);
+  }
+  parseMessage(message) {
+    this.state.parseMessage(message);
+  }
+  loadSubmissionElements() {
+    document.getElementById("woosh_audio").play();
+    $("body").addClass("hide-overflow");
+    setTimeout(() => {
+      $("body").removeClass("hide-overflow");
+    }, 1000);
+    $(".meme-format-container").append(ElementCreate.meme(this.memes[0]));
+    this.$board.append(ElementCreate.answerCard());
+  }
+  addHiddenSubmission(answer, playerName) {
+    document.getElementById("bleep_audio").play();
+    let player = this.getPlayerByName(playerName);
+    let card = new Card(answer, player, this);
+    player.submissionCard = card;
+    $(".meme-answer-container").append(card.element);
+    $(".vote-prompt").removeClass("hidden");
+  }
+  loadVotingElements() {
+    let t = 2000,
+      dt = 4000;
+    this.players.forEach((player) => {
+      setTimeout(() => {
+        player.submissionCard.reveal();
+      }, t);
+      t += dt;
+    });
+  }
+  clearVotingElements() {
+    $(".round-title").remove();
+    $(".meme-format").remove();
+    $(".reveal-card").remove();
+    $(".vote-prompt").addClass("hidden");
+    $("#answer-input").remove();
+    $("#round-overlay").css("left", "-100%").hide();
+  }
+  sendServerMessage(OUT_MSG) {
+    this.socket.emit("messageFromClient", OUT_MSG);
+  }
 }
